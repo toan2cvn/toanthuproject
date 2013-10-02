b7841e882866df8206a5888de54efe3e11047c25
nguyentoan1212@gmail.com <nguyentoan1212@gmail.com@933fd49b-dccd-e577-ae8c-14a7bf0bf00e>
2013-10-02 10:18:32 +0000

    Rangeの式と値を取得 ⇒テストメソッド

---
+++ CSharpStudy/ExcelCompareTool/ExcelComparer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExcelCompareTool
+{
+    public class ExcelComparer
+    {
+        /// <summary>
+        /// 比較元リーダー
+        /// </summary>
+        private ExcelReader exelReader1 { get; set; }
+
+        /// <summary>
+        /// 比較先リーダー
+        /// </summary>
+        private ExcelReader exelReader2 { get; set; }
+
+        /// <summary>
+        /// エラーが発生したリスト
+        /// </summary>
+        private List<List<string>> errorList { get; set; }
+
+        /// <summary>
+        /// Excel比較
+        /// </summary>
+        /// <param name="fileName1">比較元ファイル名</param>
+        /// <param name="fileName2">比較先ファイル名</param>
+        /// <param name="gridData">対象列データ</param>
+        /// <returns></returns>
+        public bool CompareExcel(string fileName1, string fileName2, List<List<string>> gridData)
+        {
+            return false;
+        }
+    }
+}
