57253d741788808022f7d88e7c15cbcb3a9a12d7
nguyentoan1212@gmail.com <nguyentoan1212@gmail.com@933fd49b-dccd-e577-ae8c-14a7bf0bf00e>
2013-10-04 04:57:31 +0000

    CSVWriter追加 Itemクラス追加

---
+++ trunk/CSharpStudy/ExcelCompareTool/InputItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExcelCompareTool
+{
+    public class InputItem : CommonItem
+    {
+        /// <summary>
+        /// 比較元Range
+        /// </summary>
+        public string Range1 { get; set; }
+
+        /// <summary>
+        /// 比較先Range
+        /// </summary>
+        public string Range2 { get; set; }
+
+        public InputItem(List<string> rowData)
+        {
+            this.No = int.Parse(rowData[0]);
+            this.ItemName = rowData[1];
+            this.SheetName = rowData[2];
+            this.Range1 = rowData[3];
+            this.Range2 = rowData[4];
+            this.Type = rowData[5];
+        }
+    }
+}
---
+++ trunk/CSharpStudy/ExcelCompareTool/ResultItem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExcelCompareTool
+{
+    public class ResultItem : CommonItem
+    {
+        /// <summary>
+        /// 比較元のセル
+        /// </summary>
+        public string Cell1 { get; set; }
+
+        /// <summary>
+        /// 比較元の値
+        /// </summary>
+        public string Value1 { get; set; }
+
+        /// <summary>
+        /// 比較先のセル
+        /// </summary>
+        public string Cell2 { get; set; }
+
+        /// <summary>
+        /// 比較先の値
+        /// </summary>
+        public string Value2 { get; set; }
+
+        /// <summary>
+        /// 差異あるかないか
+        /// </summary>
+        public bool IsDiff { get; set; }
+    }
+}
